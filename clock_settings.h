a70e9e9d3e27f3307379d3b9666a72cfbf9466a7
n0m1s <n0m1s@homnomnom.fr>
2020-01-11 23:23:20 +0100

    clock settings getters

--- clock_settings.h
+++ clock_settings.h
@@ -9,6 +9,9 @@ class ClockSettings
 
         void add_info(char const* key, char const* val);
 
+        inline char const*const server() const {return m_server;}
+        inline unsigned int cooldown() const {return m_cooldown;}
+
     protected:
         char const* m_server;
         unsigned int m_cooldown;
